3f26a4a2aaca12aff336192b56459cda5f30212f
Mariage-Helene-Jimmy <helene.chassagneux@gmail.com>
2026-06-30 17:15:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,78 @@
-document.addEventListener("DOMContentLoaded", () => {
+const URL_GOOGLE = "https://script.google.com/macros/s/AKfycbwbewX0fgK_A8_FHDOv91LBZklfkJoBblRms92i12iNj5P0WMjeBd1Id3Vo3y0BxSdf/exec";
 
-    const form = document.querySelector("form");
+let tableChoisie = localStorage.getItem("table");
 
-    form.addEventListener("submit", function(event) {
 
-        event.preventDefault();
+// Affichage de la table
+if (tableChoisie) {
+  document.getElementById("table").innerHTML =
+    "🎉 Vous êtes : " + tableChoisie;
 
-        alert(
-            "🎉 Merci pour votre participation !\n\n" +
-            "Vos photos seront bientôt envoyées."
-        );
+  chargerScore();
+  chargerDefis();
+}
+
+
+// Charger le score
+function chargerScore() {
+
+  fetch(`${URL_GOOGLE}?action=score&table=${tableChoisie}`)
+    .then(response => response.json())
+    .then(data => {
+
+      document.getElementById("score").innerHTML =
+        "Score : " + data.score + " points";
 
     });
+}
 
-});
+
+// Charger les défis
+function chargerDefis() {
+
+  fetch(`${URL_GOOGLE}?action=defis`)
+    .then(response => response.json())
+    .then(defis => {
+
+      let zone = document.getElementById("defis");
+
+      zone.innerHTML = "";
+
+      defis.forEach(defi => {
+
+        zone.innerHTML += `
+          <div class="defi">
+
+            <h3>${defi.nom}</h3>
+
+            <p>${defi.points} points</p>
+
+            <button onclick="validerDefi(${defi.points})">
+              Défi réalisé !
+            </button>
+
+          </div>
+        `;
+
+      });
+
+    });
+}
+
+
+// Ajouter les points
+function validerDefi(points) {
+
+  fetch(
+    `${URL_GOOGLE}?action=ajouter&table=${tableChoisie}&points=${points}`
+  )
+  .then(response => response.json())
+  .then(data => {
+
+    document.getElementById("score").innerHTML =
+      "Score : " + data.score + " points";
+
+    alert("Bravo ! +" + points + " points 🎉");
+
+  });
+}
